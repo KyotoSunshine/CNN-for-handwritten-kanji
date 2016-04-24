68c1633c85615ff38eb1e01ecfbdc387fd03d98d
KyotoSunshine <KyotoSunshine@users.noreply.github.com>
2016-04-25 00:07:42 +0900

    Create model_for_augmented_dataset_training_log.py

---
+++ logs/model_for_augmented_dataset_training_log.py
@@ -0,0 +1,55 @@
+Train on 400000 samples, validate on 10000 samples
+Epoch 1/10
+3083s - loss: 1.7197 - acc: 0.5893 - val_loss: 0.3504 - val_acc: 0.8959
+Epoch 2/10
+3086s - loss: 0.6644 - acc: 0.8074 - val_loss: 0.2832 - val_acc: 0.9182
+Epoch 3/10
+3093s - loss: 0.6163 - acc: 0.8242 - val_loss: 0.2946 - val_acc: 0.9136
+Epoch 4/10
+3090s - loss: 0.5857 - acc: 0.8328 - val_loss: 0.2763 - val_acc: 0.9226
+Epoch 5/10
+3088s - loss: 0.5736 - acc: 0.8369 - val_loss: 0.3469 - val_acc: 0.9054
+Epoch 6/10
+3089s - loss: 0.5639 - acc: 0.8398 - val_loss: 0.4478 - val_acc: 0.8754
+Epoch 7/10
+3084s - loss: 0.5604 - acc: 0.8415 - val_loss: 0.2732 - val_acc: 0.9238
+Epoch 8/10
+3087s - loss: 0.5526 - acc: 0.8437 - val_loss: 0.2785 - val_acc: 0.9182
+Epoch 9/10
+3126s - loss: 0.5494 - acc: 0.8446 - val_loss: 0.2387 - val_acc: 0.9328
+Epoch 10/10
+3102s - loss: 0.5444 - acc: 0.8463 - val_loss: 0.2883 - val_acc: 0.9247
+Train on 400000 samples, validate on 10000 samples
+Epoch 1/10
+3104s - loss: 0.1369 - acc: 0.9600 - val_loss: 0.0778 - val_acc: 0.9810
+Epoch 2/10
+3091s - loss: 0.0835 - acc: 0.9748 - val_loss: 0.0743 - val_acc: 0.9811
+Epoch 3/10
+3090s - loss: 0.0696 - acc: 0.9788 - val_loss: 0.0754 - val_acc: 0.9808
+Epoch 4/10
+3093s - loss: 0.0624 - acc: 0.9805 - val_loss: 0.0708 - val_acc: 0.9822
+Epoch 5/10
+3092s - loss: 0.0594 - acc: 0.9819 - val_loss: 0.0761 - val_acc: 0.9819
+Epoch 6/10
+3094s - loss: 0.0570 - acc: 0.9827 - val_loss: 0.0831 - val_acc: 0.9806
+Epoch 7/10
+3099s - loss: 0.0550 - acc: 0.9830 - val_loss: 0.0845 - val_acc: 0.9809
+Epoch 8/10
+3099s - loss: 0.0521 - acc: 0.9841 - val_loss: 0.0782 - val_acc: 0.9819
+Epoch 9/10
+Epoch 00008: early stopping
+3124s - loss: 0.0516 - acc: 0.9841 - val_loss: 0.0787 - val_acc: 0.9836
+Train on 400000 samples, validate on 10000 samples
+Epoch 1/10
+3109s - loss: 0.0293 - acc: 0.9913 - val_loss: 0.0705 - val_acc: 0.9852
+Epoch 2/10
+3105s - loss: 0.0213 - acc: 0.9938 - val_loss: 0.0713 - val_acc: 0.9853
+Epoch 3/10
+3122s - loss: 0.0180 - acc: 0.9949 - val_loss: 0.0715 - val_acc: 0.9850
+Epoch 4/10
+3107s - loss: 0.0165 - acc: 0.9954 - val_loss: 0.0731 - val_acc: 0.9855
+Epoch 5/10
+3105s - loss: 0.0146 - acc: 0.9959 - val_loss: 0.0737 - val_acc: 0.9852
+Epoch 6/10
+Epoch 00005: early stopping
+3122s - loss: 0.0134 - acc: 0.9962 - val_loss: 0.0740 - val_acc: 0.9848
